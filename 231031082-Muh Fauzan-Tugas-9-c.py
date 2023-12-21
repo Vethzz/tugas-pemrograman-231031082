2288060776e99084f8ca4457882a0a81542e7e89
Fauzaannn <153783210+Fauzaannn@users.noreply.github.com>
2023-12-21 19:00:24 +0800

    Add files via upload

---
+++ 231031082-Muh Fauzan-Tugas-9-c.py
@@ -0,0 +1,17 @@
+vd = {"nama" : "Muh Fauzan",
+      "kelas" : "SI23-C",
+      "nim" : "231031082",
+      "asal" : "Mamuju",
+      "tanggal" : "16-07-2005",
+      "alamat" : "jl ratulangi",
+      "hobi" : "Main game",
+      "tinggi" : "172 cm",
+      }
+print("Nama Lengkap  :",vd["nama"])
+print("Kelas         :",vd["kelas"])
+print("NIM           :",vd["nim"])
+print("Tmpt tgl lahir:",vd["asal"],vd["tanggal"])
+print("Alamat        :",vd["alamat"])
+print("Hobi          :",vd["hobi"])
+print("Tinggi        :",vd["tinggi"])
+print("\nTipe data var vd : ",type(vd))
